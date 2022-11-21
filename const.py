cb0e4bd2d07f9d8b79b2bc6801257865e82db35f
Rafael Medrado <72144146+CMedrado@users.noreply.github.com>
2022-11-21 14:51:18 -0300

    update const bob and alice

--- const.py
+++ const.py
@@ -2,4 +2,4 @@ CHAT_SERVER_HOST = "172.31.27.176"
 CHAT_SERVER_PORT = 5001
 
 # IDs de usuários registrados com os respectivos endereços IP e números de porta
-registry = {"Alice":("10.128.0.2",5002), "Bob":("10.128.0.4",5002)}
+registry = {"Alice":("172.31.27.176",5002), "Bob":("172.31.27.176",5002)}
